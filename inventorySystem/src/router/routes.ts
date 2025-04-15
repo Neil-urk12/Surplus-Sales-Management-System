0ff9b2af73a80acfd8317b03f53b3cd522ad21c5
Marc Ejay <ejaycortes8@gmail.com>
2025-04-15 21:46:05 +0800

    initital commit

--- inventorySystem/src/router/routes.ts
+++ inventorySystem/src/router/routes.ts
@@ -3,19 +3,27 @@ import type { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    redirect: '/login' // 👈 redirect root to login
+  },
+  {
+    path: '/login',
+    component: () => import('pages/LoginPage.vue'),
+  },
+  {
+    path: '/app',
     component: () => import('layouts/MainLayout.vue'),
-    children: [ { path: '', component: () => import('pages/DashboardPage.vue')},
-                { path: 'inventory', component: () => import('pages/InventoryPage.vue') },
-                { path: 'sales', component: () => import('pages/SalesPage.vue') },
-                { path: 'contacts', component: () => import('pages/ContactsPage.vue') }
+    meta: { requiresAuth: true },
+    children: [
+      { path: '', component: () => import('pages/DashboardPage.vue') },
+      { path: 'inventory', component: () => import('pages/InventoryPage.vue') },
+      { path: 'sales', component: () => import('pages/SalesPage.vue') },
+      { path: 'contacts', component: () => import('pages/ContactsPage.vue') },
     ],
   },
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
   },
-];
+]
 
 export default routes;
